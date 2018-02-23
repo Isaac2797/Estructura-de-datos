e18c3452422d299d05f6c7556b279eca12375b36
Isaac2797 <36213348+Isaac2797@users.noreply.github.com>
2018-02-23 11:27:24 -0600

    Add files via upload

---
+++ Practica_Bits_y_Bytes/Practica_Bits_y_Bytes/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Practica_Bits_y_Bytes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.dtp1 = new System.Windows.Forms.DateTimePicker();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnDate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(160, 23);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Procesar";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(35, 25);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(193, 162);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(100, 20);
+            this.txtDate.TabIndex = 6;
+            // 
+            // dtp1
+            // 
+            this.dtp1.Location = new System.Drawing.Point(73, 19);
+            this.dtp1.Name = "dtp1";
+            this.dtp1.Size = new System.Drawing.Size(193, 20);
+            this.dtp1.TabIndex = 7;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(122, 97);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 8;
+            // 
+            // btnDate
+            // 
+            this.btnDate.Location = new System.Drawing.Point(103, 58);
+            this.btnDate.Name = "btnDate";
+            this.btnDate.Size = new System.Drawing.Size(130, 23);
+            this.btnDate.TabIndex = 9;
+            this.btnDate.Text = "Ajustar Fecha";
+            this.btnDate.UseVisualStyleBackColor = true;
+            this.btnDate.Click += new System.EventHandler(this.btnDate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtp1);
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.btnDate);
+            this.groupBox1.Location = new System.Drawing.Point(57, 188);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(332, 174);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fecha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Sensor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Sensor 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "FechaLectura";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Nivel";
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(229, 89);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(64, 50);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb3.TabIndex = 16;
+            this.pb3.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(97, 89);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(55, 50);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 11;
+            this.pb2.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(35, 89);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(56, 50);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 10;
+            this.pb1.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(347, 89);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(58, 50);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb4.TabIndex = 18;
+            this.pb4.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(353, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Dirección";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.Location = new System.Drawing.Point(194, 146);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(110, 13);
+            this.lblLevel.TabIndex = 20;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 371);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.DateTimePicker dtp1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnDate;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblLevel;
+    }
+}
+
---
+++ Practica_Bits_y_Bytes/Practica_Bits_y_Bytes/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Bits_y_Bytes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(txtNumber.Text);
+
+            selectingBits(num);
+            selectingDate(num);
+
+        }
+
+        int selectingBits(int num)
+        {
+            num >>= 1;
+            int direction = num & 7; //Comparar valor con 00000111
+            num >>= 3;
+
+            int level = num & 3; //Comparar valor con 00000011
+            num >>= 2;
+
+            int Sensor2 = num & 1; //Comparar valor con 00000001
+            num >>= 1;
+
+            int Sensor1 = num & 1; //Comparar valor con 00000001
+
+            return showInfo(direction, level, Sensor2, Sensor1);
+        }
+
+        int selectingDate(int num)
+        {
+            num >>= 8;
+
+            int day = num & 31; //comparar valor con 00011111
+            num >>= 5;
+            if (day == 0)
+            {
+                MessageBox.Show("Dia cambiará a lunes");
+                day = 1;
+            }
+
+            int month = num & 15; //comparar valor con 00001111
+            if (month > 12)
+            {
+                MessageBox.Show("Mes cambiará a Diciembre");
+                month = 12;
+            }
+            else if (month == 0)
+            {
+                MessageBox.Show("Mes cambiará a enero");
+                month = 1;
+            }
+            num >>= 4;
+
+            int year = num & 127; // comparar valor con 01111111
+            year += 1900;
+
+            DateTime date = new DateTime(year, month, day);
+
+            string fulldate = String.Format("{0:dd/MM/yyyy}", date);
+
+            
+
+            txtDate.Text = fulldate;
+
+            return 0;
+            
+        }
+
+        long changeDate(DateTime dt)
+        {
+            int y = dt.Year-1900;
+            int m = dt.Month;
+            int d = dt.Day;
+            int res = y;
+            res <<= 4;
+            res += m;
+            res <<= 5;
+            res += d;
+           
+            txtResult.Text = Convert.ToString(res);
+
+            return 0;
+        }
+
+        int showInfo(int d, int l, int s2, int s1)
+        {
+            if(s1 == 1)
+            {
+                this.pb1.Image = Practica_Bits_y_Bytes.Properties.Resources.SEncendido;
+            }
+            else
+            {
+                this.pb1.Image = Practica_Bits_y_Bytes.Properties.Resources.SApagado;
+            }
+
+            if (s2 == 1)
+            {
+                this.pb2.Image = Practica_Bits_y_Bytes.Properties.Resources.SEncendido;
+            }
+            else
+            {
+                this.pb2.Image = Practica_Bits_y_Bytes.Properties.Resources.SApagado;
+            }
+
+            switch(l)
+            {
+                case 0:
+                    this.pb3.Image = Practica_Bits_y_Bytes.Properties.Resources.Empty;
+                    lblLevel.Text = "Vacio";
+                    break;
+
+                case 1:
+                    this.pb3.Image = Practica_Bits_y_Bytes.Properties.Resources.Half_full;
+                    lblLevel.Text = "Nivel medio";
+                    break;
+
+                case 2:
+                    this.pb3.Image = Practica_Bits_y_Bytes.Properties.Resources.Full;
+                    lblLevel.Text = "Lleno";
+                    break;
+
+                case 3:
+                    this.pb3.Image = Practica_Bits_y_Bytes.Properties.Resources.Enproceso;
+                    lblLevel.Text = "Proceso de llenado";
+                    break;
+                   
+            }
+
+            switch(d)
+            {
+                case 0: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.N; break;
+
+                case 1: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.NE; break;
+
+                case 2: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.E; break;
+
+                case 3: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.SE; break;
+
+                case 4: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.S; break;
+
+                case 5: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.SO; break;
+
+                case 6: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.O; break;
+
+                case 7: this.pb4.Image = Practica_Bits_y_Bytes.Properties.Resources.NO; break;
+            }
+
+            return 0;
+        }
+
+        private void btnDate_Click(object sender, EventArgs e)
+        {
+
+            changeDate(dtp1.Value);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
