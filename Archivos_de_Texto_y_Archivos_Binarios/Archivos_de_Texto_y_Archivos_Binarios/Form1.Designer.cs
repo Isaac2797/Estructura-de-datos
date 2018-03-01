f3895f1daaffc68a81ec33927c9174b189e79ee2
Isaac2797 <36213348+Isaac2797@users.noreply.github.com>
2018-02-28 21:09:43 -0600

    Add files via upload

---
+++ Archivos_de_Texto_y_Archivos_Binarios/Archivos_de_Texto_y_Archivos_Binarios/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Archivos_de_Texto_y_Archivos_Binarios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtBmpdata = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtCellphone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(255, 34);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(107, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtBmpdata
+            // 
+            this.txtBmpdata.Location = new System.Drawing.Point(118, 116);
+            this.txtBmpdata.Multiline = true;
+            this.txtBmpdata.Name = "txtBmpdata";
+            this.txtBmpdata.Size = new System.Drawing.Size(166, 88);
+            this.txtBmpdata.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "BMP data";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(104, 39);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(104, 65);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(104, 91);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 12;
+            // 
+            // txtCellphone
+            // 
+            this.txtCellphone.Location = new System.Drawing.Point(104, 117);
+            this.txtCellphone.Name = "txtCellphone";
+            this.txtCellphone.Size = new System.Drawing.Size(100, 20);
+            this.txtCellphone.TabIndex = 13;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(104, 143);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Phone Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Age";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Correo";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(248, 89);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 19;
+            this.btnCreate.Text = "Create File";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 146);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Address";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtCellphone);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(32, 223);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 186);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "XML File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 462);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBmpdata);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtBmpdata;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtCellphone;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Archivos_de_Texto_y_Archivos_Binarios/Archivos_de_Texto_y_Archivos_Binarios/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace Archivos_de_Texto_y_Archivos_Binarios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Busca la imagen dentro del directorio con un filtro de solo imagenes bmp
+        /// </summary>
+        OpenFileDialog openFile = new OpenFileDialog
+        {
+            InitialDirectory = Directory.GetCurrentDirectory(),
+            Filter = "image file (*.bmp)|*.bmp",
+            RestoreDirectory = true,
+        };
+
+        /// <summary>
+        /// Guarda el archivo en el directorio con un filtro de XML
+        /// </summary>
+        SaveFileDialog saveFile = new SaveFileDialog
+        {
+            InitialDirectory = Directory.GetCurrentDirectory(),
+            Filter = "XML-File|*.xml"
+        };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                FileStream archive = new FileStream(openFile.FileName, FileMode.Open);
+                byteSearch(archive);
+                openImage();
+                archive.Close();
+            }
+        }
+
+        /// <summary>
+        /// Muestra los datos del archivo seleccionado
+        /// </summary>
+        /// <param name="archive"></param>
+        /// <returns></returns>
+        int byteSearch(FileStream archive)
+        {
+            BinaryReader br = new BinaryReader(archive);
+            archive.Seek(14, SeekOrigin.Begin);
+            int header = br.ReadInt32();
+
+            if (header == 40)
+            {
+                archive.Seek(0, SeekOrigin.Begin);
+                string code = new string(br.ReadChars(2));
+                if (code == "BM")
+                {
+                    archive.Seek(2, SeekOrigin.Begin);
+                    int size = br.ReadInt32();
+
+                    archive.Seek(12, SeekOrigin.Current);
+                    int width = br.ReadInt32();
+
+                    int height = br.ReadInt32();
+
+                    archive.Seek(2, SeekOrigin.Current);
+                    int bitperpixel = br.ReadInt16();
+
+                    txtBmpdata.Text = "Real size: " + size.ToString() + "\r\n"
+                        + "Width: " + width.ToString() + "\r\n"
+                        + "Height: " + height.ToString() + "\r\n"
+                        + "Bits per pixel: " + bitperpixel.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Not a bmp Image");
+                    pictureBox1 = null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Not a bmp Image");
+                pictureBox1 = null;
+            }
+            br.Close();
+
+            return 0;
+
+        }
+
+        /// <summary>
+        /// Abre la imagen(archivo) seleccionada y lo muestra en el picturebox
+        /// </summary>
+        /// <returns></returns>
+        int openImage()
+        {
+            try
+            {
+                FileStream imageArchive = new FileStream(openFile.FileName, FileMode.Open);
+                pictureBox1.Image = Image.FromStream(imageArchive);
+                imageArchive.Close();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed loading image");
+            }
+            return 0;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            createFile();
+        }
+
+        /// <summary>
+        /// Creo el archivo XML
+        /// </summary>
+        public void createFile()
+        {
+            saveFile.ShowDialog();
+            FileStream fs = new FileStream(saveFile.FileName, FileMode.Create);
+
+            StreamWriter sw = new StreamWriter(fs);
+
+
+            string chain = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+                + "<Registration>"
+                + "<data>"
+                + "<name>" + txtName.Text + "</name>"
+                + "<email>" + txtEmail.Text + "</email>"
+                + "<age>" + txtAge.Text + "</age>"
+                + "<cellphone>" + txtCellphone.Text + "</cellphone>"
+                + "<address>" + txtAddress.Text + "</address>"
+                + "</data>"
+                + "</Registration>";
+
+            sw.Write(chain);
+            sw.Close();
+            fs.Close();
+
+        }
+    }
+}
